6f6449b9949e47d48db58a5bbf6d1d8cb33bdfb1
KieranCoopr <kieran.cooper@students.plymouth.ac.unknown>
2021-11-26 17:02:11 +0000

    creted a way to paint

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,54 @@
+//Global Variables
+var brushSize, brushShape, paintColour;
+var coloursArray = ['red','green','blue'];
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1280, 720);
     canvas.parent("p5container");
+    
+    brushSize = 10;
+     background(220);
+
+     paintColour = coloursArray[0,1,2];
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if(paintColour === 'red'){
+        fill('rgb(255,0,0)');
+    }
+    else if (paintColour === 'green'){
+        fill('rgb(0,225,0)')
+    }
+    else if (paintColour === 'blue'){
+        fill('rgb(0,0,225)')
+    }
+
+    noStroke();
+    
+    if(mouseIsPressed == true ){
+        ellipse(mouseX, mouseY, brushSize);
+
+    };
+    
+}
+
+
+function keyPressed(){
+if(keyCode === UP_ARROW && brushSize <= 50){
+    brushSize = brushSize + 5;
+} 
+else if(keyCode === DOWN_ARROW && brushSize >= 10){
+   brushSize = brushSize - 5;
+}
+if(keyCode === 81){
+    paintColour = coloursArray[0];
+}
+if(keyCode === 87){
+    paintColour = coloursArray[1];
+}
+if(keyCode === 69){
+    paintColour = coloursArray[2];
+}
+
 }
